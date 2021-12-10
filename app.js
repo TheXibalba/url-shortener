0f4e351e75245d1bbb5ad4b3179e1c0ef08f86b8
Yuvraj Pawar <yurajpawar99@gmail.com>
2021-12-11 00:28:33 +0530

    Testing Atlas

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser");
 const indexRouter = require("./routes/indexRouter");
 const bitlyRouter = require("./routes/bitlyRouter");
 const userRouter = require("./routes/userRouter");
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -14,10 +15,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 app.use(cookieParser());
 
-mongoose.connect("mongodb://localhost/url_shortner", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost/url_shortner", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+const DB_USERNAME=process.env.DB_USERNAME;
+const DB_PASSWORD=process.env.DB_PASSWORD;
+const DB_NAME=process.env.DB_NAME;
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.ia2jk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((data) => {
+    console.log("Database has been connected!");
+  })
+  .catch((err) => {
+    console.log("Data connection error!");
+  });
 
 app.use("/", indexRouter);
 app.use("/user", userRouter);
