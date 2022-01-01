596649d9111d5c0df9b4ad39c099c5f9c61c1852
Yuvraj Pawar <yurajpawar99@gmail.com>
2022-01-01 20:03:23 +0530

    minor changes

--- routes/bitlyRouter.js
+++ routes/bitlyRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bitlyController = require("../controllers/bitlyController");
 const mongoose = require("mongoose");
-const User = require("../models/User");
+const User = require("../models/user");
 const axios = require("axios");
 
 router.post("/shorten", async (req, res) => {
